0421ae2c2c10fe9c3b0bb7f7f883a1ddfb75dca3
Derek Mohr <derek@mightyinthemidwest.com>
2021-08-07 15:10:35 -0400

    Establish Events list template

---
+++ page-events.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The template for displaying a page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package Highstarter
+ *
+ * @since 1.0
+ * @version 1.0
+ * @copyright  Copyright (c) 2020, Atanas Yonkov
+ * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ */
+
+// template controlled by tribe/events
