06578925f7458e0366ac14785117c6d2b72de4d2
Derek Mohr <derek@mightyinthemidwest.com>
2021-08-28 18:58:46 -0400

    [#2] Add src, laravel-mix

---
+++ src/scripts/modules.js
@@ -0,0 +1,9 @@
+// =============================================================================
+// Modules
+// =============================================================================
+// Set all necessary modules here that we will need to watch for throughout the
+// front end.
+
+// Modules exports
+// =============================================================================
+// export {default as Example} from './modules/Example';
